7817270462b029ac66b60d042207309217717e94
sdlima4025 <sergiolimafg3@gmail.com>
2023-06-07 11:20:14 -0300

    GET com Fetch Api / Filmes em Cartaz.

---
+++ requisicoesGet/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { Movie } from "./types/Movie";
+
+const App = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  // load filmes com useEfect Function
+  useEffect(() => {
+    loadMovies();
+  });
+  const loadMovies = () => {
+    fetch("https://api.b7web.com.br/cinema/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setMovies(json);
+      });
+  };
+  return (
+    // load filmes com onClick function loadMovies.
+    <div>
+      {/* <button className="block bg-blue-400 p-2" onClick={loadMovies}>
+        Carregar Filmes
+      </button> */}
+      Total de Filmes: {movies.length}
+      <div className="grid grid-cols-6 gap-3">
+        {movies.map((item, index) => (
+          <div key={index}>
+            <img src={item.avatar} className="w-32 block" />
+            {item.titulo}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
